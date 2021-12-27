d76c6196619822af441a4056b0bd19ad1d3b79db
Michael.Kryvoruchko <Michael.Kryvoruchko@mdm.de>
2021-12-27 17:30:51 +0200

    upgrade

--- packages/typescript/index.js
+++ packages/typescript/index.js
@@ -1,7 +1,12 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const basic = require('@gitart/eslint-config-basic')
+
 module.exports = {
   extends: [
+    '@gitart/eslint-config-basic',
     'plugin:@typescript-eslint/recommended',
   ],
+  overrides: basic.overrides,
   rules: {
     'import/named': 'off',
 
